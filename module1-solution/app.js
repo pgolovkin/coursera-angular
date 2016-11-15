e5b7d78cfea9298ee0cec07f16ca46d2fb7a4fa4
Головкин Павел < >
2016-11-15 21:47:56 +0300

    Module 1 solution

---
+++ module1-solution/app.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+    .controller('LunchCheckController',LunchCheckController);
+    LunchCheckController.$inject = ['$scope'];
+    function LunchCheckController($scope) {
+      $scope.lunchMessages = "";
+      $scope.lunchMenu = "";
+      $scope.checkLunch = function() {
+        if ($scope.lunchMenu == "") {
+          $scope.lunchMessages = "Please enter data first";
+          return;
+        }
+        var lunchMenu = $scope.lunchMenu.split(',').length;
+        if (lunchMenu <=3 ) {
+          $scope.lunchMessages = "Enjoy!";
+        } else {
+          $scope.lunchMessages = "Too much!";
+        }
+      }
+    }
+})();
