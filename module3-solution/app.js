a9938aef94259584d43ced096f7db96bce17443e
Головкин Павел Леонидович <pgolovkin@bscmsc.ru>
2016-11-22 23:35:51 +0300

    module 3

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,14 +3,63 @@
 
       angular.module('NarrowItDownApp', [])
         .controller('NarrowItDownController', NarrowItDownController)
-        .service('MenuSearchService', MenuSearchService);
+        .service('MenuSearchService', MenuSearchService)
+        .directive('foundItems', FoundItemsDirective)
+        .constant('ApiBasePath', 'http://davids-restaurant.herokuapp.com');
 
       NarrowItDownController.$inject = ['MenuSearchService'];
       function NarrowItDownController(MenuSearchService) {
-        
+        var menu = this;
+
+        menu.getMatchedMenuItems = function(searchTerm) {
+           var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+           promise.then(function (result) {
+             menu.found = result;
+           });
+         }
       };
 
-      function MenuSearchService() {
+
+      MenuSearchService.$inject = ['$http', 'ApiBasePath']
+      function MenuSearchService($http, ApiBasePath) {
+        var service = this;
+
+        service.getMatchedMenuItems = function(searchTerm) {
+          return $http({
+            method: "GET",
+            url: (ApiBasePath + "/menu_items.json")
+          }).then(function (result) {
+            var foundItems = [];
+            var array = result.data.menu_items;
+            for (var i = 0; i < array.length; i++) {
+              if (array[i].description.indexOf(searchTerm) !== -1) {
+                foundItems.push(array[i]);
+              }
+            }
+            console.log(foundItems);
+            return foundItems;
+          })
+        };
+      }
+
+
+      function FoundItemsDirective() {
+        var ddo = {
+          templateUrl: 'foundItemList.html',
+          scope: {
+            items: "<items",
+          },
+          controller: FoundListDirectiveController,
+          controllerAs: 'list',
+          bindToController: true,
+          transclude: true
+        };
+
+        return ddo;
+      };
+
+      function FoundListDirectiveController() {
+        var controller = this;
 
       }
 })();
